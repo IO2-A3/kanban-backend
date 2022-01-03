f75bcd0c2b40f7060cbf2e9c820677cad7d3ab55
Pawel80256 <paweuq2@yahoo.pl>
2022-01-03 20:19:49 +0100

    naprawa buga z taskami w listach

--- src/main/java/com/example/kanbanbackend/task/TaskRepository.java
+++ src/main/java/com/example/kanbanbackend/task/TaskRepository.java
@@ -4,8 +4,11 @@ import com.example.kanbanbackend.task.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    Set<Task> findByListId(UUID listId);
 }
