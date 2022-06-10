789c0b34a49c18c332dc75c3021c4bec2155e1f4
Lukasz Szulborski <lukasz-szulborski@o2.pl>
2022-06-10 18:59:07 +0200

    return ID of the single project

--- src/main/java/com/example/kanbanbackend/project/models/ProjectIdDto.java
+++ src/main/java/com/example/kanbanbackend/project/models/ProjectIdDto.java
@@ -7,12 +7,14 @@ import lombok.Setter;
 
 import java.sql.Timestamp;
 import java.util.Set;
+import java.util.UUID;
 
 @Getter
 @Setter
 public class ProjectIdDto {
     private Timestamp createdAt;
     private String name;
+    private UUID id;
     private Set<ListSetDto> listSet;
     private Set<ProjectMemberRole> projectMemberRoles;
 }
